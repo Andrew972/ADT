8903b7d7a2c698e4e78c0eb00244d3836dffe7f0
Fatem <f.s4remi@gmail.com>
2020-04-03 04:03:52 -0700

    creating a Client obj

---
+++ Code_base/Client.java
@@ -0,0 +1,168 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Client {
+    private final int ID;
+    private Name name;
+    private HAddress addy;
+    private Health specs;
+    private String phone;
+    private String emergencyPhone;
+
+    private ArrayList<Integer> householdMembers;
+
+    Scanner scanner = new Scanner(System.in);
+
+   //copy the given things or allow to go through set up?
+    public Client() {
+        this.ID = generateID();
+        setAddress();
+
+        setName();
+        
+        setSpecs();
+        phone = "NA";
+        emergencyPhone = "NA"; 
+        householdMembers = new ArrayList<Integer> ();
+    }
+
+    public static int generateID(){
+        int value = 100;
+        return value++;
+    }
+
+    public static void main(String[] args) {
+        Client myself = new Client();
+    }
+    public ArrayList<Integer> getHouseholdMembers() {
+        return this.householdMembers;
+    }
+
+    private void setAddress() {
+        System.out.println("Adding address to your account...");
+        addy = new HAddress(); // NA object
+
+        int stNumber, zip;
+        String stName, apt, state;
+
+        System.out.println("Enter street number:");
+        stNumber = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Enter street name:");
+        stName = scanner.nextLine();
+
+        System.out.println("Enter apartment number or NA if none:");
+        apt = scanner.nextLine();
+
+        System.out.println("Enter state:");
+        state = scanner.nextLine();
+
+        System.out.println("Enter zipcode:");
+        zip = scanner.nextInt();
+        scanner.nextLine();
+
+        addy.streetNumber(stNumber)
+            .streetName(stName)
+            .apt(apt)
+            .state(state)
+            .zipCode(zip);
+    }
+
+    private void setName() {
+        System.out.println("Setting personal information...");
+        name = new Name();
+
+        String fName,lName;
+        System.out.println("Enter your first name: ");
+        fName = scanner.nextLine();
+
+        System.out.println("Enter your last name: ");
+        lName = scanner.nextLine();
+
+        name.firstName(fName)
+            .lastName(lName);
+    }
+
+    private void setSpecs() {
+        System.out.println("Adding more personal information...");
+        specs = new Health();
+        
+        int age, weight, height;  
+        String gender;
+         
+        System.out.println("Enter your age:");
+        age = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Enter your weight:");
+        weight = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Enter your height:");
+        height = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Enter your gender:");
+        gender = scanner.nextLine();
+
+        specs.age(age)
+            .gender(gender)
+            .height(height)
+            .weight(weight);
+    }
+
+    public Client phone(String phone) {
+        this.phone = phone;
+        return this;
+    }
+
+    public Client emergencyPhone(String emergencyPhone) {
+        this.emergencyPhone = emergencyPhone;
+        return this;
+    }
+
+    public Client addHouseholdMembers(int houseMember) {
+        if(! householdMembers.contains(houseMember)){
+            householdMembers.add(houseMember);
+        }
+    
+        return this;
+    }
+
+    public HAddress getAddy() {
+        return this.addy;
+    }
+
+    public Name getName() {
+        return this.name;
+    }
+
+    public Health getSpecs() {
+        return this.specs;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public String getEmergencyPhone() {
+        return this.emergencyPhone;
+    }
+
+    public int getID() {
+        return this.ID;
+    }
+
+    @Override
+    public String toString() {
+        return "address= " + getAddy() + " " +
+            ", name= " + getName() + " " +
+            ", specs= " + getSpecs() + " " +
+            ", phone= " + getPhone() + " " +
+            ", emergencyPhone= " + getEmergencyPhone() + " " +
+            ", ID= " + getID() + " " +
+            ", householdMembers= " + getHouseholdMembers();
+    }
+
+}
