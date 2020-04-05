782b1f280d2fb74af5dbd141a26533f2e8c5054b
Fatem <f.s4remi@gmail.com>
2020-04-04 17:47:21 -0700

    added signin method

--- Code_base/ClientDB.java
+++ Code_base/ClientDB.java
@@ -12,6 +12,42 @@ public class ClientDB {
         numberOfClients = 0;
     }
 
+    public void signin(){
+        String possibleUserName;
+        boolean athenticated = false;
+        String possiblePassword;
+        System.out.println("Enter your usrname: ");
+        possibleUserName = scanner.nextLine();
+        var result = lookUpUserName(possibleUserName);
+        if(result != null ){
+            int tries = 0;
+            while(tries <= 3 && athenticated){
+                System.out.println("Enter your password: ");
+                possiblePassword = scanner.nextLine();
+                
+                if(possiblePassword.equals(result.getPassword())){
+                    System.out.println("You're signed in.");
+                    athenticated = true;
+                    break;
+                }
+                else{
+                    System.out.println("That's incorrect. Try again: ");
+                }
+            }
+        }
+        else{
+            System.out.println("No account with that username was found.");
+        }
+    }
+
+    private Client lookUpUserName(String userID){
+        for( int i = 0; i < clients.size(); i++){
+            if( userID.equals(clients.get(i).getUserName())){
+                return clients.get(i);
+            }
+        }
+        return null;
+    }
     public void signup(){
         Client newMember = new Client();
 
