ef41d56dbda09ba7d91702341f00c2a69602570b
Andrew Chappel <ilovemavs200@yahoo.com>
2020-04-14 18:13:48 -0700

    Updated hospital to reflect change for inheritance

--- Phase5_Monitor/Hospital.java
+++ Phase5_Monitor/Hospital.java
@@ -1,16 +1,9 @@
 import java.util.Random;
 
-public class Hospital 
+public class Hospital extends EmergencyStation
 {
-	private Random randomNumber = new Random();
-	
 	public Hospital()
 	{
 		
-	}
-	
-	int sendEta(String userInfo)
-	{
-		return randomNumber.nextInt(120-1+1)+1;
 	}
 }
