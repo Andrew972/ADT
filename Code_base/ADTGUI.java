33ee67a97883d7c62c5db8025296205c03d412d4
Fatem <f.s4remi@gmail.com>
2020-04-16 02:23:51 -0700

    Removed code for signup, less cluttered

--- Code_base/ADTGUI.java
+++ Code_base/ADTGUI.java
@@ -2,12 +2,8 @@ import javax.swing.JFrame;
 
 public class ADTGUI extends JFrame {
 
-    // a panel that will be attached to the top of the frame
-    private SignUpUserPassword getUserPassWord;
-    private SignUpName getUserName;
-    
-    private Monitor adtBackendMonitor = new Monitor();
-    private Message signUp = new Message();
+    //**** Register Button****
+    // panels that will be attached to the top of the frame. 
 
     ADTGUI(){
         super("JBK ADT Security");
@@ -15,45 +11,16 @@ public class ADTGUI extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         setLocationRelativeTo(null);
-        
-        getUserPassWord = new SignUpUserPassword();
-        add(getUserPassWord);
-
-
-        getUserPassWord.setListener(new ComponentListener(){
-			public void informationEmitted(Message info) {
-                //add information to the obj that will eventually get sent back to monitor
-                signUp.addOn(info);
-                setNamePanel();
-            }
-        });
         setVisible(true);
     }
 
-    private void setNamePanel(){
-        this.remove(getUserPassWord);
-        getUserName = new SignUpName();
-        add(getUserName);
-
-        getUserName.setListener(new ComponentListener(){
-			public void informationEmitted(Message info) {
-                //add information to the obj that will eventually get sent back to monitor
-                signUp.addOn(info);
-                setAddressPanel();
-            }
-        });
-
-        revalidate();
+    // Displays all of the forms related to regitering /sign up a new client
+    private void showRegister(){
+        Register registterPages = new Register(this);
     }
-
-    private void setAddressPanel(){
-        
-        System.out.println("Hi from address panel");
-        for (String key : signUp.content.keySet()) {
-            System.out.println("key: " + key + " value: " + signUp.get(key));
-        }
-    }
+   
     public static void main(String[] args) {
         ADTGUI gui = new ADTGUI();
+        gui.showRegister();
     }
 }
