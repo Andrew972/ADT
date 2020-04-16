c8cf206208c011c065b9fa542ac5e99ecd51f27a
Fatem <f.s4remi@gmail.com>
2020-04-15 20:46:12 -0700

    figured out how to change panels on the main frame

--- Code_base/ADTGUI.java
+++ Code_base/ADTGUI.java
@@ -4,7 +4,10 @@ public class ADTGUI extends JFrame {
 
     // a panel that will be attached to the top of the frame
     private SignUpUserPassword getUserPassWord;
+    private SignUpName getUserName;
+    
     private Monitor adtBackendMonitor = new Monitor();
+    private Message signUp = new Message();
 
     ADTGUI(){
         super("JBK ADT Security");
@@ -16,19 +19,41 @@ public class ADTGUI extends JFrame {
         getUserPassWord = new SignUpUserPassword();
         add(getUserPassWord);
 
+
         getUserPassWord.setListener(new ComponentListener(){
 			public void informationEmitted(Message info) {
-                //send the info to the monitor
-                //being a new panel
-                System.out.println("hello from ADTGUI");
-                adtBackendMonitor.createNewUser(info);
+                //add information to the obj that will eventually get sent back to monitor
+                signUp.addOn(info);
+                setNamePanel();
             }
         });
         setVisible(true);
     }
 
+    private void setNamePanel(){
+        this.remove(getUserPassWord);
+        getUserName = new SignUpName();
+        add(getUserName);
+
+        getUserName.setListener(new ComponentListener(){
+			public void informationEmitted(Message info) {
+                //add information to the obj that will eventually get sent back to monitor
+                signUp.addOn(info);
+                setAddressPanel();
+            }
+        });
+
+        revalidate();
+    }
+
+    private void setAddressPanel(){
+        
+        System.out.println("Hi from address panel");
+        for (String key : signUp.content.keySet()) {
+            System.out.println("key: " + key + " value: " + signUp.get(key));
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hi");
         ADTGUI gui = new ADTGUI();
     }
 }
