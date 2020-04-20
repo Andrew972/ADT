1120ae09c4cb9c416bdc9e5814e5589a82253f84
Andrew Chappel <ilovemavs200@yahoo.com>
2020-04-20 15:36:43 -0700

    Window door sensor updated

---
+++ Code_base/WindowDoorSensor.java
@@ -0,0 +1,56 @@
+public class WindowDoorSensor implements SensorInter {
+    Boolean alarmStatus;
+    String locationInHouse;
+
+
+    public WindowDoorSensor(){
+        locationInHouse = "unknown";
+        alarmStatus = false;
+    }
+
+    public WindowDoorSensor(String location){
+        this.locationInHouse = location;
+        this.alarmStatus = false;
+    }
+
+    public WindowDoorSensor(String location, Boolean alarm){
+        this.locationInHouse = location;
+        this.alarmStatus = alarm;
+    }
+
+    public void setLocation(String location){
+        this.locationInHouse = location;
+    }
+
+    @Override
+    public void setAlarmStatus(Boolean alarmStatus) {
+        this.alarmStatus = alarmStatus;
+    }
+
+    public Boolean checkStatus() {
+        int testNumber = generateRandNum();
+        if (testNumber == 5) {
+            alarmStatus = true;
+        }
+
+        if(alarmStatus == true) {
+            alarmStatus = false; //reset alarm, we've already recorded it was triggered
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    private int generateRandNum() {
+        int temp = (int) ((Math.random() * 25));
+        return temp;
+
+    }
+
+    public String getStatusLocation(){
+        return locationInHouse;
+    }
+
+
+}
