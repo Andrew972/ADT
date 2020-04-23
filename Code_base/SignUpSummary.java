b56dc0d9dccdf09bf4eb5d379fdd66ed6573db79
Andrew Chappel <ilovemavs200@yahoo.com>
2020-04-23 15:45:41 -0700

    New format to be uniform with other sign up panels

--- Code_base/SignUpSummary.java
+++ Code_base/SignUpSummary.java
@@ -5,7 +5,8 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class SignUpSummary extends JPanel {
+public class SignUpSummary extends JPanel 
+{
     private JLabel title = new JLabel("Here is the informaiton we have");
     private JLabel username = new JLabel("USERNAME:");
     private JLabel pass = new JLabel("PASSWORD:");
@@ -13,13 +14,13 @@ public class SignUpSummary extends JPanel {
     private JLabel name = new JLabel("NAME:");
     private ourFont writingFont;
     private JButton editButton, submitButton;
-
     private ComponentListener listener;
-
+    private GridBagConstraints panel = new GridBagConstraints();
+    
     public SignUpSummary(Message info){
         super();
         setSize(375,700);
-
+        setLayout(new GridBagLayout());
         writingFont = new ourFont(14);
         name.setFont(writingFont);
         title.setFont(writingFont);
@@ -37,25 +38,49 @@ public class SignUpSummary extends JPanel {
         });
 
         submitButton = new JButton("submit");
-        submitButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("clicked submit to go back to sign in");
+        
+        submitButton.addActionListener(new ActionListener() 
+        {
+            public void actionPerformed(ActionEvent e) 
+            {
+            	 listener.informationEmitted(info);
             }
         });
 
-        add(title);
-        add(username);
-        add(new JLabel(info.get("username")));
-        add(pass);
-        add(new JLabel(info.get("password")));
-        add(name);
-        add(new JLabel(info.get("firstName")+" " + info.get("lastName")));
-        add(address);
-        add(new JLabel(info.get("number")+" " + info.get("name")+" "+info.get("city")+" "+info.get("state")+" "+info.get("zip")));
+        panel.gridx = 3;
+        panel.gridy = 4;
+        add(title,panel);
+        panel.gridx = 3;
+        panel.gridy = 5;
+        add(username,panel);
+        panel.gridx = 3;
+        panel.gridy = 6;
+        add(new JLabel(info.get("username")),panel);
+        panel.gridx = 3;
+        panel.gridy = 7;
+        add(pass,panel);
+        panel.gridx = 3;
+        panel.gridy = 8;
+        add(new JLabel(info.get("password")),panel);
+        panel.gridx = 3;
+        panel.gridy = 9;
+        add(name,panel);
+        panel.gridx = 3;
+        panel.gridy = 10;
+        add(new JLabel(info.get("firstName")+" " + info.get("lastName")),panel);
+        panel.gridx = 3;
+        panel.gridy = 11;
+        add(address,panel);
+        panel.gridx = 3;
+        panel.gridy = 12;
+        add(new JLabel(info.get("number")+" " + info.get("name")+" "+info.get("city")+" "+info.get("state")+" "+info.get("zip")),panel);
+        panel.gridx = 3;
+        panel.gridy = 13;
+        add(submitButton,panel);
     }
 
     public void setListener(ComponentListener l)
 	 {
-	    listener = l;
+	        listener = l;
 	 }
 }
