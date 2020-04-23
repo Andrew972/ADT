86f3647fbd3bd8989c603c89abf40dbd201bd20a
Andrew Chappel <ilovemavs200@yahoo.com>
2020-04-23 15:46:27 -0700

    Reformat to remain uniform with other sign in panels

--- Code_base/SignUpUserPassword.java
+++ Code_base/SignUpUserPassword.java
@@ -7,61 +7,64 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 public class SignUpUserPassword extends JPanel {
-    private JLabel user,pass;
-	private JTextField userName, password;
+    private JLabel user;
+	private JLabel pass;
+	private JTextField userName;
+    private JTextField password;
     private JButton nextButton;
     private ComponentListener listener;
-    private ourFont writingFont;
-
+    private GridBagConstraints panel = new GridBagConstraints();
     //needs a photo
     //content needs to be centered in the window, probably BoxLayout works better
 	
     public SignUpUserPassword() {
         super();
-        writingFont = new ourFont(14);
         nextButton = new JButton("Next");
         setSize(375,700);
-        setLayout(new FlowLayout());
+        setLayout(new GridBagLayout());
 
         user = new JLabel("Choose a username");
-        user.setFont(writingFont);
         userName = new JTextField(20);
         pass = new JLabel("Choose a password");
-        pass.setFont(writingFont);
         password = new JTextField(20);
         
-        add(user);
-        add(userName);
-        add(pass);
-        add(password);
-
-        add(nextButton);
+        panel.gridx = 5;
+        panel.gridy = 5;
+        add(user, panel);
+        
+        panel.gridx = 5;
+        panel.gridy = 6;
+        add(userName, panel);
+        
+        panel.gridx = 5;
+        panel.gridy = 7;
+        add(pass, panel);
+        
+        panel.gridx = 5;
+        panel.gridy = 8;
+        add(password, panel);
+        
+        panel.gridx = 5;
+        panel.gridy = 9;
+        add(nextButton,panel);
+        
         nextButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                if(validateUserInput() ){
-                    Message info = new Message();
-                    info.addContent("username", userName.getText().toLowerCase().trim());
-                    info.addContent("password", password.getText());
-    
-                    listener.informationEmitted(info);
-                }
+                Message info = new Message();
+                info.addContent("username", userName.getText().toLowerCase());
+                
+                info.addContent("password", password.getText());
+
+                listener.informationEmitted(info);
             }
         });
     }
 
     public void setListener(ComponentListener l){
         listener = l;
-    }
-    
-    private boolean validateUserInput(){
-        if( userName.getText().length() == 0){
-            return false;
-        }
-        if( password.getText().length() == 0){
-            return false;
-        }
-        return true;
     }
 }
