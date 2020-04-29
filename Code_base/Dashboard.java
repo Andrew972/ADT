baca4575c46543ce82ee256ab70127c8c44c97d2
Andrew Chappel <ilovemavs200@yahoo.com>
2020-04-29 11:56:35 -0700

    Updated dashboard with scenario buttons and rearragned status panels

--- Code_base/Dashboard.java
+++ Code_base/Dashboard.java
@@ -28,56 +28,51 @@ public class Dashboard extends JPanel
 
 
 	
-	private JButton sensor;
-	private JButton logOut;
-	private JButton lockDoor;
-	private Dimension imgSize= new Dimension(100,100);
-	private ImageIcon lockImage;
-	private ImageIcon sensorImage;
-	private JPanel lockDoorStatusPanel;
-	private JPanel lockWindowStatusPanel;
-	private JPanel smokeAlarmStatusPanel;
-	private JPanel coDetectorStatusPanel;
+	private JButton sensor,logOut,lockDoor,fireButton,policeButton,hospitalButton,
+	dashBoard,sos,news;
+	private Dimension imgSize= new Dimension(70,70);
+	private ImageIcon lockImage,sensorImage,logOffImage,fireImage,policeImage,hospitalImage;
+	private JPanel lockDoorStatusPanel,lockWindowStatusPanel,smokeAlarmStatusPanel,coDetectorStatusPanel;
 	private ComponentListener listener;
-	
-	
-	
 	private GridBagConstraints panel = new GridBagConstraints();
 	private Border blackline = BorderFactory.createLineBorder(Color.black);
-	private Border raisedetched = BorderFactory.createEtchedBorder(EtchedBorder.RAISED);
-	private Border loweredbevel = BorderFactory.createLoweredBevelBorder();
-	private Border compound = BorderFactory.createCompoundBorder(raisedetched, loweredbevel);
-	
 	
 	public Dashboard(JFrame mainFrame)
 	{
 		super(); 
-		
 		setLayout(new GridBagLayout());
 		setSize(375,700);
-		panel.insets = new Insets(5,30,5,0);
-		panel.gridx = 1;
-		panel.gridy = 10;
+		panel.insets = new Insets(5,15,5,5);
+		panel.gridx = 6;
+		panel.gridy = 12;
 		add(CreateLockDoorButton(),panel);
-		panel.gridx = 1;
-		panel.gridy = 11;
+		panel.gridx = 6;
+		panel.gridy = 13;
 		add(CreateSensorButton(),panel);
+		panel.gridx = 6;
+		panel.gridy = 14;
+		add(CreateLogOutButton(),panel);
 		panel.gridx = 1;
 		panel.gridy = 12;
-		add(CreateLogOutButton(),panel);
-		panel.gridx = 0;
-		panel.gridy = 9;
 		add(createDoorStatusPanel(),panel);
-		panel.gridx = 0;
-		panel.gridy = 10;
-		add(createWindowStatusPanel(),panel);
-		panel.gridx = 0;
-		panel.gridy = 11;
-		add(createSmokeStatusPanel(),panel);
-		panel.gridx = 0;
+		panel.gridx = 10;
 		panel.gridy = 12;
+		add(createWindowStatusPanel(),panel);
+		panel.gridx = 1;
+		panel.gridy = 14;
+		add(createSmokeStatusPanel(),panel);
+		panel.gridx = 10;
+		panel.gridy = 14;
 		add(createCoStatusPanel(),panel);
-		
+		panel.gridx = 9;
+		panel.gridy = 12;
+		add(CreateFireButton(),panel);
+		panel.gridx = 9;
+		panel.gridy = 13;
+		add(CreatePoliceButton(),panel);
+		panel.gridx = 9;
+		panel.gridy = 14;
+		add(CreateHospitalButton(),panel);
 		
 		
 		mainFrame.add(this, BorderLayout.WEST);
@@ -94,8 +89,6 @@ public class Dashboard extends JPanel
 		coDetectorStatusPanel.setBorder(smokeBorder);
 		coDetectorStatusPanel.setBackground(Color.GREEN);
 		return coDetectorStatusPanel;
-		
-		
 	}
 	
 	private JPanel createSmokeStatusPanel()
@@ -106,7 +99,6 @@ public class Dashboard extends JPanel
 		smokeAlarmStatusPanel.setBorder(smokeBorder);
 		smokeAlarmStatusPanel.setBackground(Color.GREEN);
 		return smokeAlarmStatusPanel;
-		
 	}
 	
 	private JPanel createDoorStatusPanel()
@@ -127,21 +119,17 @@ public class Dashboard extends JPanel
 		lockWindowStatusPanel.setPreferredSize(new Dimension(62,62));
 		lockWindowStatusPanel.setBackground(Color.GREEN);
 		lockWindowStatusPanel.setBorder(windowBorder);
-		
 		return lockWindowStatusPanel;
 	}
 	
 	private JButton CreateLockDoorButton()
 	{
-		
 		lockImage = new ImageIcon("Lock1.jpg");
 		Image firstImage = lockImage.getImage();
-		Image secondImage = firstImage.getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH);
+		Image secondImage = firstImage.getScaledInstance(70, 70, java.awt.Image.SCALE_SMOOTH);
 		lockImage = new ImageIcon(secondImage);
-		
 		lockDoor = new JButton(lockImage);
 		lockDoor.setPreferredSize(imgSize);
-		
 		lockDoor.addActionListener(new ActionListener() 
 		{
 			 public void actionPerformed(ActionEvent e) 
@@ -149,23 +137,17 @@ public class Dashboard extends JPanel
 				
 	         }
 	     });
-		
-		
 		return lockDoor;
-		
 	}
 
 	private JButton CreateSensorButton()
 	{
-		
-		
 		sensorImage = new ImageIcon("sensor.png");
 		Image firstImage = sensorImage.getImage();
-		Image secondImage = firstImage.getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH);
+		Image secondImage = firstImage.getScaledInstance(70, 70, java.awt.Image.SCALE_SMOOTH);
 		sensorImage = new ImageIcon(secondImage);
 		sensor = new JButton(sensorImage);
 		sensor.setPreferredSize(imgSize);
-		
 		sensor.addActionListener(new ActionListener() 
 		{
 			 public void actionPerformed(ActionEvent e) 
@@ -174,16 +156,17 @@ public class Dashboard extends JPanel
 	         
 	         }
 	     });
-		
-		
 		return sensor;
 	}
 
 	private JButton CreateLogOutButton()
 	{
-
-		logOut = new JButton("Log out");
-		
+		logOffImage = new ImageIcon("logout.png");
+		Image firstImage = logOffImage.getImage();
+		Image secondImage = firstImage.getScaledInstance(70, 70, java.awt.Image.SCALE_SMOOTH);
+		logOffImage = new ImageIcon(secondImage);
+		logOut = new JButton(logOffImage);
+		logOut.setPreferredSize(imgSize);
 		logOut.addActionListener(new ActionListener() 
 		{
 			 public void actionPerformed(ActionEvent e) 
@@ -191,14 +174,68 @@ public class Dashboard extends JPanel
 				 listener.informationEmitted(null);
 	         }
 	     });
-		
-		
 		return logOut;
 	}
 
+	
+	private JButton CreateFireButton()
+	{
+		fireImage = new ImageIcon("fireIcon.jpg");
+		Image firstImage = fireImage.getImage();
+		Image secondImage = firstImage.getScaledInstance(70, 70, java.awt.Image.SCALE_SMOOTH);
+		fireImage = new ImageIcon(secondImage);
+		fireButton = new JButton(fireImage);
+		fireButton.setPreferredSize(imgSize);
+		fireButton.addActionListener(new ActionListener() 
+		{
+			 public void actionPerformed(ActionEvent e) 
+	         {
+				 listener.informationEmitted(null);
+	         }
+	     });
+		return fireButton;
+	}
+	
+	
+	private JButton CreatePoliceButton()
+	{
+		policeImage = new ImageIcon("police.png");
+		Image firstImage = policeImage.getImage();
+		Image secondImage = firstImage.getScaledInstance(70, 70, java.awt.Image.SCALE_SMOOTH);
+		policeImage = new ImageIcon(secondImage);
+		policeButton = new JButton(policeImage);
+		policeButton.setPreferredSize(imgSize);
+		policeButton.addActionListener(new ActionListener() 
+		{
+			 public void actionPerformed(ActionEvent e) 
+	         {
+				 listener.informationEmitted(null);
+	         }
+	     });
+		return policeButton;
+	}
+	
+	private JButton CreateHospitalButton()
+	{
+		hospitalImage = new ImageIcon("hospital.png");
+		Image firstImage = hospitalImage.getImage();
+		Image secondImage = firstImage.getScaledInstance(70, 70, java.awt.Image.SCALE_SMOOTH);
+		hospitalImage = new ImageIcon(secondImage);
+		hospitalButton = new JButton(hospitalImage);
+		hospitalButton.setPreferredSize(imgSize);
+		hospitalButton.addActionListener(new ActionListener() 
+		{
+			 public void actionPerformed(ActionEvent e) 
+	         {
+				 listener.informationEmitted(null);
+	         }
+	     });
+		return hospitalButton;
+	}
+	
 	public void setListener(ComponentListener l)
-	 {
-	        listener = l;
-	 }
+	{
+	    listener = l;
+	}
 
 }
