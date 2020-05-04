e1613aeb41f806261ea0c1258077886a851ae878
Andrew Chappel <ilovemavs200@yahoo.com>
2020-05-03 23:58:01 -0700

    Updated the overall gui to accomidate the change of the dashboard panels to be classes

--- Code_base/ADTGUI.java
+++ Code_base/ADTGUI.java
@@ -1,18 +1,11 @@
-import java.awt.Image;
-
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class ADTGUI extends JFrame {
-
-    //**** Register Button****
-    // panels that will be attached to the top of the frame. 
-
-	private Monitor tempMonitor = new Monitor();
-
-	ADTGUI()
-    {
+    private Monitor adtBackendMonitor;
+    ADTGUI(){
         super("JBK ADT Security");
+        adtBackendMonitor = new Monitor();
+
         setSize(375, 700);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
@@ -21,81 +14,47 @@ public class ADTGUI extends JFrame {
     }
 
     // Displays all of the forms related to regitering /sign up a new client
-    public void showRegister()
-    {
-    	Register registterPages = new Register(this);
-    	
-    	registterPages.setListener(new ComponentListener() 
-    	{
+    private void showRegister(){
+        Register registterPages = new Register(this);
+        
+        registterPages.setListener(new ComponentListener() {
 			public void informationEmitted(Message info) 
 			{
-				tempMonitor.createNewUser(info);
-				getContentPane().removeAll();
-				showPurchasePackages();
-			}
-    	});
-   }
-   
-    public static void main(String[] args) 
-    {
-        ADTGUI gui = new ADTGUI();
-        //gui.showSignIn();
-        //gui.showSos();
-        gui.showDash();
-    }
-
-    
-    private void showSos()
-    {
-    
-    	SosPanel sos = new SosPanel(this);
-    
-    }
-    
-    private void showPurchasePackages()
-    {
-    	PurchasePackages purchase = new PurchasePackages(this);
-    	
-    	purchase.setListener(new ComponentListener() 
-    	{
-			public void informationEmitted(Message info) 
-			{
-				tempMonitor.purchasepackage(info);
-				getContentPane().removeAll();
-				showSignIn();
+				if (info.get("Action").equals("Validate")){
+					registterPages.useernameIsValid(adtBackendMonitor.validateUsername(info));					
+				}
+				else{
+					adtBackendMonitor.createNewUser(info);
+					getContentPane().removeAll();
+					showSignIn();
+				}
 			}
     	});
     }
     
-    private void showSignIn()
-    {
+    private void showSignIn(){
     	SignInPanel tempPanel = new SignInPanel(this);
-    	
-    	tempPanel.setListener(new ComponentListener() 
-    	{
-			public void informationEmitted(Message info) 
-			{
-				if(info.get("Action") == "Register")
-				{
+
+    	tempPanel.setListener(new ComponentListener() {
+			public void informationEmitted(Message info){
+				if(info.get("Action").equals("Register")){
 					getContentPane().removeAll();
 					showRegister();
 				}
 				
-				else if(info.get("Action") == "Dash")
+				else if(info.get("Action").equals("Dash"))
 				{
-					if(tempMonitor.signIn(info) != -1)
+					System.out.println(adtBackendMonitor.signIn(info));
+					if(adtBackendMonitor.signIn(info) != -1)
 					{
 						getContentPane().removeAll();
 						showDash();
 					}
-				
-					else
-					{
+					else{
 						getContentPane().removeAll();
 						showErrorMessage();
 					}
 				}
-				
 			}
     	});
     	
@@ -103,47 +62,59 @@ public class ADTGUI extends JFrame {
 
     private void showDash()
     {
-    	Dashboard dashBoardView = new Dashboard(this);
-    
-    	dashBoardView.setListener(new ComponentListener() 
-    	{
-			public void informationEmitted(Message info) 
-			{
-				getContentPane().removeAll();
-				showSignIn();
+		Dashboard dashBoardView = new Dashboard(this);
+		dashBoardView.setListener(new ComponentListener(){
+			public void informationEmitted(Message info) {
+				if(info.get("Action").equals("Arm")){
+					dashBoardView.setMode(SysMode.ARM);
+				}
+				else if(info.get("Action").equals("Disarm")){
+					dashBoardView.setMode(SysMode.DISARM);
+				}
 			}
-    	});
+		});
     }
 
-    private void showErrorMessage()
+    private void showPurchasePackages(){
+        PurchasePackages packageChoice = new PurchasePackages(this);
+        packageChoice.setListener(new ComponentListener(){
+            public void informationEmitted(Message info) {
+				adtBackendMonitor.purchaseAlarmPackage(info); 
+				getContentPane().removeAll();
+                showSignIn();         
+            }
+        });
+    }
+
+   private void showErrorMessage()
     {
     	ErrorSignIn errorPanel = new ErrorSignIn(this);
     	errorPanel.setListener(new ComponentListener() 
     	{
 			public void informationEmitted(Message info) 
 			{
-				if(info.get("Action") == "Register")
-				{
+				if(info.get("Action") == "Register"){
 					getContentPane().removeAll();
 					showRegister();
 				}
 				
-				else if(info.get("Action") == "Dash")
-				{
-					if(tempMonitor.signIn(info) != -1)
+				else if(info.get("Action") == "Dash"){
+					if(adtBackendMonitor.signIn(info) != -1)
 					{
 						getContentPane().removeAll();
 						showDash();
 					}
 				
-					else
-					{
-						getContentPane().removeAll();
-						showErrorMessage();
-					}
+					getContentPane().removeAll();
+					showErrorMessage();
 				}
 				
 			}
     	});
     }
+    
+    public static void main(String[] args) {
+        ADTGUI gui = new ADTGUI();
+		gui.showDash();
+	}
 }
