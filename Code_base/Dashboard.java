c52970c9b0fe00a1bf830da2ee776e262974cae2
Andrew Chappel <ilovemavs200@yahoo.com>
2020-05-07 00:36:30 -0700

    Updated dashboard to better comadate the button bar

--- Code_base/Dashboard.java
+++ Code_base/Dashboard.java
@@ -5,25 +5,28 @@ import java.awt.Color;
 import javax.swing.BoxLayout;
 public class Dashboard extends JPanel {
 	private Toppanel top;
-	private Middlepanel middle;
+	private Middlepanel middle;;
 	private Buttonspanel buttBar;
-	private ComponentListener listener;
-	private SosPanel SOSPanel; 
-	
+	private ComponentListener listener; 
+	private NewsFeed newsFeedInstance;
+
 	public Dashboard(JFrame mainFrame) {
 		super();
 		setSize(375,700);
 		setBackground(Color.WHITE); 
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
+		NewsFeed newsFeed = new NewsFeed();
+		newsFeedInstance = newsFeed.getInstance();
+
 		top = new Toppanel();
+		top.setUpArmDisarmView();
 		middle = new Middlepanel();
 		buttBar = new Buttonspanel();
 
 		add(top);
 		add(middle);
 		add(buttBar);
-
 		
 		top.setListener(new ComponentListener(){
 			public void informationEmitted(Message info) {
@@ -31,19 +34,22 @@ public class Dashboard extends JPanel {
 			}
 		});
 
-		buttBar.setListener(new ComponentListener()
-		{
-			public void informationEmitted(Message info) 
-			{
-				if(info.get("Action").equals("Sos"))
-				{
-					SOSPanel = new SosPanel();
-					removeAll();
-					add(SOSPanel);
-					add(buttBar);
-					revalidate();
-					repaint();
-					
+		buttBar.setListener(new ComponentListener(){
+			public void informationEmitted(Message info) { 
+				if(info.get("Action").equals("Sos")){
+					top.setUpSOSView();
+					//middle.setUpETAView();
+				}
+				else if(info.get("Action").equals("Dash")){
+					middle.SetSensorPanel();
+					top.setUpArmDisarmView();
+				}
+				else if(info.get("Action").equals("Logout")){
+					listener.informationEmitted(info);
+				}
+				else{//Actions is News
+					middle.setNewsPanel(newsFeed.displayNews());
+					top.setUpArmDisarmView();
 				}
 			}
 		});
@@ -53,7 +59,7 @@ public class Dashboard extends JPanel {
 
 	public void setListener(ComponentListener l){
         listener = l;
-	}
+	}  
 	
 	public void setMode(SysMode M){
 		if(M == SysMode.ARM) top.setArm();
