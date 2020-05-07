dc234c5934c14fb340f817d2e10d0cc36d7ef6b4
Andrew Chappel <ilovemavs200@yahoo.com>
2020-05-07 16:07:40 -0700

    Updated so the error message can be displayed properly

--- Code_base/ErrorSignIn.java
+++ Code_base/ErrorSignIn.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.GridBagConstraints;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -6,12 +7,16 @@ import javax.swing.JLabel;
 public class ErrorSignIn extends SignInPanel 
 {
 	private JLabel errorLabel = new JLabel("Wrong password or username, please try again");
-
+	private GridBagConstraints panel = new GridBagConstraints();
+	
 	public ErrorSignIn(JFrame mainFrame)
 	{
 		super(mainFrame);
+		panel.gridx = 1;
+		panel.gridy = 0;
+		
 		errorLabel.setForeground(Color.RED);
-		add(errorLabel);
+		add(errorLabel,panel);
 		
 	}
 
